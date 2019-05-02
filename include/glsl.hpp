f259a3bacd4f7def02d1e9c2ae97adc023164de8
MsrOhkwr <m.ohkawara@keio.jp>
2019-05-03 00:35:25 +0900

    introduce App class

--- include/glsl.hpp
+++ include/glsl.hpp
@@ -1,12 +1,12 @@
-#ifndef GLSL_H_
-#define GLSL_H_
+#ifndef GLSL_HPP_
+#define GLSL_HPP_
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 void framebuffer_size_callback(GLFWwindow* window, const int width, const int height);
 void scroll_callback(GLFWwindow* window, const double x, const double y);
-int readShaderSource(const GLuint shader, const char *filename);
+int readShaderSource(const GLuint shader, const std::string filename);
 void printShaderInfoLog(const GLuint shader);
 void printProgramInfoLog(const GLuint program);
 void processInput(GLFWwindow *window);
